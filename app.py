6937652b2c42131d741208504039cc3093ecd42e
Anil Yerramasu <anilyerramasu@Anils-MacBook-Pro.local>
2025-01-12 19:42:37 +0530

    Auth added

--- app.py
+++ app.py
@@ -122,18 +122,16 @@ async def set_starters():
         ]
 # ...
 
-# @cl.password_auth_callback
-# def auth_callback(username: str, password: str):
-#     # Fetch the user matching username from your database
-#     # and compare the hashed password with the value stored in the database
-#     username1 = os.getenv("USERNAME")
-#     password1 = os.getenv("PASSCODE")
-#     if (username, password) == (username1, password1) :
-#         return cl.User(
-#             identifier="admin", metadata={"role": "admin", "provider": "credentials"}
-#         )
-#     else:
-#         return None
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
 
 @cl.on_chat_start
 async def on_chat_start():
